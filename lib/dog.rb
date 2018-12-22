670f245f1715f4fc5ca9fa7eb92f4195f3837a14
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-22 10:17:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,6 +73,7 @@ class Dog
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
+  end
 
 #   def self.find_or_create_by(name:, breed:)
 #     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?AND breed = ?", name, breed)
