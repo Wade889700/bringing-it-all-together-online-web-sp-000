7aa4c42dea5773615ee94378a5ea8b29bc089eb6
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-22 10:01:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,8 +45,12 @@ class Dog
     new_dog.save
   end
 
-  def self.new_from_db(dog_hash)
-    Dog.new(dog_hash[0], dog_hash[1], dog_hash[2])
+  def self.new_from_db(row)
+    new_dog = Dog.new
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+    new_dog
   end
 
   def self.find_by_name(name)
