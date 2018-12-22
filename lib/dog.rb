eb5cad775937568e4098b4f67690525009d0d778
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-22 10:02:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,11 +46,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = Dog.new
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.breed = row[2]
-    new_dog
+    self.new(name: row[1], breed: row[2], id: row[0])
   end
 
   def self.find_by_name(name)
