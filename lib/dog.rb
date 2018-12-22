68b3e54dff5a91b171da0c8f272d655d408b2343
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-22 09:58:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ class Dog
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM dogs WHERE name = ?
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
