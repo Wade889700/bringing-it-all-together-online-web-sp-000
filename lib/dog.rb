51e647e92a230169e32c9263eab7b260d6aee8d6
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-22 09:54:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,12 @@ class Dog
     new_dog.save
   end
 
-  def self.find_by_name 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name) 
   end
 
 
