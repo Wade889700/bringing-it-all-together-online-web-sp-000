2acaf4aea0e45d0ac4a38ff8fd40bfec038c57ff
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-22 09:49:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,14 +28,14 @@ class Dog
   end
 
   def save
-      sql = <<-SQL
-        INSERT INTO dogs(name, breed) VALUES (?, ?)
-      SQL
+    sql = <<-SQL
+      INSERT INTO dogs(name, breed) VALUES (?, ?)
+    SQL
 
-      result = DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      return self
-    end
+    result = DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    return self
+  end
 
 
 end
